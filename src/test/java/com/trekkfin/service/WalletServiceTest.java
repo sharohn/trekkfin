9bb066136d44418f5047f34cc0ae21b02cb24e66
sharohn <rohanpop87@gmail.com>
2026-06-01 18:39:37 +0530

    Added test profile for gihub build

--- src/test/java/com/trekkfin/service/WalletServiceTest.java
+++ src/test/java/com/trekkfin/service/WalletServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.util.concurrent.CountDownLatch;
@@ -20,6 +21,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class WalletServiceTest {
 
     @Autowired
